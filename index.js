2047455ef41bef6e28fc0258963340c84b4c139d
Marwa <marwahakmi@gmail.com>
2022-07-29 16:33:25 +0300

    Lab Completed

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+
+let currentUser = "Jonh Smith"
+
+currentUser = "Jad Sarrot"
+
+let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!"
+
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!"
+
+let shortGreeting = "Welcome, " + currentUser[0] + "!"
+
